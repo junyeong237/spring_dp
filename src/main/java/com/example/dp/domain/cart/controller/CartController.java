b16874f9b98a2168ad92a7da08d7a6e17b0c69d7
juneyoung Lee <junyeong237@gmail.com>
2023-12-06 15:38:43 +0900

    [#17] Feat : CartController 조회메서드(임시) 추가

---
+++ src/main/java/com/example/dp/domain/cart/controller/CartController.java
@@ -0,0 +1,29 @@
+package com.example.dp.domain.cart.controller;
+
+
+import com.example.dp.domain.cart.dto.CartResponseDto;
+import com.example.dp.domain.cart.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @GetMapping
+    public List<CartResponseDto> getCart() {
+
+        //cartService.getCart();
+        return null;//임시용
+
+    }
+
+}
