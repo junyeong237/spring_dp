5ffe5e3882df99d6df992f5f0ac3ce6454d8dc29
seastamp <seastamp10@gmail.com>
2023-12-06 11:47:28 +0900

    [#7] feat: menuCategory entity 구현

---
+++ src/main/java/com/example/dp/domain/menu/entity/MenuCategory.java
@@ -0,0 +1,38 @@
+package com.example.dp.domain.menu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "TB_MENU_CATEGORY")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MenuCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
+    @Builder
+    private MenuCategory(Category category, Menu menu){
+        this.category =  category;
+        this.menu = menu;
+    }
+}
