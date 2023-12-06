ba7b1c3083a34cfc6ac580f0b6db12be37c1c7f7
junxtar <aa1870088@naver.com>
2023-12-06 11:20:44 +0900

    [#6]refactor: UserSignUpRequest 코드 개선

--- src/main/java/com/example/dp/domain/user/dto/request/UserSignUpRequest.java
+++ src/main/java/com/example/dp/domain/user/dto/request/UserSignUpRequest.java
@@ -10,15 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSignUpRequest {
 
-    @NotBlank(message = "유저이름은 필수 값 입니다.")
     @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "유저 이름은 영 소문자, 숫자를 포함한 4글자 이상 10글자 이하입니다.")
     private String username;
 
-    @NotBlank(message = "이메일은 필수 값 입니다.")
     @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "적절한 이메일 형식이 아닙니다.")
     private String email;
 
-    @NotBlank(message = "비밀번호는 필수 값 입니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "비밀번호는 영문자, 숫자를 포함한 8글자 이상 15글자 이하입니다.")
     private String password;
 }
