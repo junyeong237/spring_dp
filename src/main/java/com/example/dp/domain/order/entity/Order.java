68f34f072da02d59ed22918973e06ea3ed29ff04
juneyoung Lee <junyeong237@gmail.com>
2023-12-06 12:48:17 +0900

    [#5] Feat : Order Entity 추가

---
+++ src/main/java/com/example/dp/domain/order/entity/Order.java
@@ -0,0 +1,40 @@
+package com.example.dp.domain.order.entity;
+
+
+import com.example.dp.domain.model.TimeEntity;
+import com.example.dp.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Getter
+@Entity
+@Table(name="order")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends TimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStateEnum state;
+
+    @Builder
+    private Order(User user, OrderStateEnum state){
+        this.user = user;
+        this.state = state;
+    }
+
+
+}
