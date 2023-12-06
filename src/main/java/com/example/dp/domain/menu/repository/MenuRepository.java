52ede25ca1cbde4d76d0ddcfcd0c1b7c816b12f7
seastamp <seastamp10@gmail.com>
2023-12-06 22:36:32 +0900

    [#16] Refactor : MenuRepository 반환값 Optional 적용

--- src/main/java/com/example/dp/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/dp/domain/menu/repository/MenuRepository.java
@@ -2,11 +2,12 @@ package com.example.dp.domain.menu.repository;
 
 import com.example.dp.domain.menu.entity.Menu;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
-    Menu findByName(String name);
+    Optional<Menu> findByName(String name);
 
     List<Menu> findAllOrderByCreatedAt();
 }
