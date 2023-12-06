95782bd06eb2e67737219a56a4816b71c0a12bf3
junxtar <aa1870088@naver.com>
2023-12-06 21:45:34 +0900

    [#27] feat: 인증 / 인가 Filter 구현

---
+++ src/main/java/com/example/dp/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,74 @@
+package com.example.dp.global.security;
+
+import com.example.dp.domain.redis.RedisUtil;
+import com.example.dp.domain.user.dto.request.UserLoginRequestDto;
+import com.example.dp.domain.user.entity.User;
+import com.example.dp.global.jwt.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final JwtUtil jwtUtil;
+    private final RedisUtil redisUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil, RedisUtil redisUtil) {
+        this.jwtUtil = jwtUtil;
+        this.redisUtil = redisUtil;
+        setFilterProcessesUrl("/api/users/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+        HttpServletResponse response) throws AuthenticationException {
+        try {
+            UserLoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(),
+                UserLoginRequestDto.class);
+            return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    requestDto.getUsername(),
+                    requestDto.getPassword(),
+                    null
+                )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+        HttpServletResponse response, FilterChain chain, Authentication authResult) {
+
+        User user = ((UserDetailsImpl) authResult.getPrincipal()).getUser();
+
+        String accessToken = jwtUtil.createAccessToken(user.getUsername(),
+            user.getRole().getAuthority());
+        String refreshToken = jwtUtil.createRefreshToken();
+
+        response.addHeader(JwtUtil.ACCESS_TOKEN_HEADER, accessToken);
+        response.addHeader(JwtUtil.REFRESH_TOKEN_HEADER, refreshToken);
+
+        refreshToken = refreshToken.split(" ")[1].trim();
+
+        redisUtil.set(refreshToken, user.getId(), 60 * 24 * 14);
+
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request,
+        HttpServletResponse response, AuthenticationException failed) {
+        response.setStatus(401);
+    }
+
+}
