4c19038491366fee6f94a814a458ef530b6a1727
seastamp <seastamp10@gmail.com>
2023-12-06 20:31:50 +0900

    [#16] Refactor : MenuController ResponseEntity 반환하도록 수정

--- src/main/java/com/example/dp/domain/menu/controller/MenuController.java
+++ src/main/java/com/example/dp/domain/menu/controller/MenuController.java
@@ -4,6 +4,7 @@ import com.example.dp.domain.menu.dto.request.MenuRequestDto;
 import com.example.dp.domain.menu.dto.response.MenuDetailResponseDto;
 import com.example.dp.domain.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,24 +22,28 @@ public class MenuController {
     private final MenuService menuService;
 
     @PostMapping("/admin/menus")
-    public MenuDetailResponseDto createMenu(@RequestBody MenuRequestDto menuRequestDto) {
-        return menuService.createMenu(menuRequestDto);
+    public ResponseEntity<MenuDetailResponseDto> createMenu(@RequestBody MenuRequestDto menuRequestDto) {
+         MenuDetailResponseDto responseDto = menuService.createMenu(menuRequestDto);
+         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/admin/menus/{menuId}")
-    public MenuDetailResponseDto updateMenu(
+    public ResponseEntity<MenuDetailResponseDto> updateMenu(
         @PathVariable Long menuId,
         @RequestBody MenuRequestDto menuRequestDto) {
-        return menuService.updateMenu(menuId, menuRequestDto);
+        MenuDetailResponseDto responseDto = menuService.updateMenu(menuId, menuRequestDto);
+        return ResponseEntity.ok(responseDto);
     }
 
     @DeleteMapping("/admin/menus/{menuId}")
-    public void deleteMenu(@PathVariable Long menuId){
+    public ResponseEntity<?> deleteMenu(@PathVariable Long menuId){
         menuService.deleteMenu(menuId);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/admin/menus/{menuId}")
-    public MenuDetailResponseDto getAdminMenu(@PathVariable Long menuId) {
-        return menuService.getAdminMenu(menuId);
+    public ResponseEntity<MenuDetailResponseDto> getAdminMenu(@PathVariable Long menuId) {
+        MenuDetailResponseDto responseDto = menuService.getAdminMenu(menuId);
+        return ResponseEntity.ok(responseDto);
     }
 }
