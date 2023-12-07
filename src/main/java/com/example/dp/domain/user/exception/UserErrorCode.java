66d9036909535a21839bf267b220a7cbfaaa8bcc
junxtar <aa1870088@naver.com>
2023-12-07 20:48:38 +0900

    [#47] feat: 유저 예외 코드 구현

---
+++ src/main/java/com/example/dp/domain/user/exception/UserErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.dp.domain.user.exception;
+
+import com.example.dp.global.exception.code.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+
+    EXISTS_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 유저이름입니다."),
+    EXISTS_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
