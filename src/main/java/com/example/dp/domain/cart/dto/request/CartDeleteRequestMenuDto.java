3f5c72cb555573e311d163c8a193377acb074c69
juneyoung Lee <junyeong237@gmail.com>
2023-12-07 12:52:32 +0900

    Refactor : Dto 수정 및 추가

--- src/main/java/com/example/dp/domain/cart/dto/request/CartDeleteRequestMenuDto.java
+++ src/main/java/com/example/dp/domain/cart/dto/request/CartDeleteRequestMenuDto.java
@@ -3,17 +3,9 @@ package com.example.dp.domain.cart.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-
 @Getter
 @AllArgsConstructor
-public class CartRequestMenuDto {
-
+public class CartDeleteRequestMenuDto {
 
     private String name; // 메뉴 이름
-
-    private Integer menuCounts; //해당 메뉴의 갯수
-
-    private Integer priceSum; //총 가격
-
-
 }
