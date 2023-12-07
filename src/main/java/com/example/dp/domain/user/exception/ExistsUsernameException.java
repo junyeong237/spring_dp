3ee9ddb642b2092ecfb71e36b873364b9c4bd399
junxtar <aa1870088@naver.com>
2023-12-07 20:49:08 +0900

    [#47] refactor: 유저 예외 처리 코드 적용

---
+++ src/main/java/com/example/dp/domain/user/exception/ExistsUsernameException.java
@@ -0,0 +1,10 @@
+package com.example.dp.domain.user.exception;
+
+import com.example.dp.global.exception.RestApiException;
+import com.example.dp.global.exception.code.ErrorCode;
+
+public class ExistsUsernameException extends RestApiException {
+    public ExistsUsernameException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
