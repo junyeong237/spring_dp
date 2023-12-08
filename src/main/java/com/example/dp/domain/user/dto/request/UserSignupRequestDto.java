db54e274724fb235f36053407e7fa4d6f5da9fa7
junxtar <aa1870088@naver.com>
2023-12-08 21:02:36 +0900

    [#79] refactor: username -> email 요구사항 변경 반영

--- src/main/java/com/example/dp/domain/user/dto/request/UserSignupRequestDto.java
+++ src/main/java/com/example/dp/domain/user/dto/request/UserSignupRequestDto.java
@@ -8,10 +8,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSignupRequestDto {
-
-    @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "유저 이름은 영 소문자, 숫자를 포함한 4글자 이상 10글자 이하입니다.")
-    private String username;
-
     @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "적절한 이메일 형식이 아닙니다.")
     private String email;
 
