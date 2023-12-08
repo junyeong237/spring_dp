fd30e4bc83373a8c865fb59cd9004dabd2960a46
phdljr <phdljr@naver.com>
2023-12-09 01:21:01 +0900

    [#88]Feat: User 프로필 이미지 수정/삭제 기능 추가

--- src/main/java/com/example/dp/domain/user/controller/UserController.java
+++ src/main/java/com/example/dp/domain/user/controller/UserController.java
@@ -3,14 +3,23 @@ package com.example.dp.domain.user.controller;
 import com.example.dp.domain.user.dto.UserCheckCodeRequestDto;
 import com.example.dp.domain.user.dto.UserSendMailRequestDto;
 import com.example.dp.domain.user.dto.request.UserSignupRequestDto;
+import com.example.dp.domain.user.dto.response.UserResponseDto;
 import com.example.dp.domain.user.service.impl.UserServiceImpl;
+import com.example.dp.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,10 +41,26 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("signup/mail/code")
+    @PostMapping("/signup/mail/code")
     public ResponseEntity<Boolean> checkCode(@RequestBody UserCheckCodeRequestDto request) {
 
         return ResponseEntity.ok(userService.checkCode(request));
+    }
+
+    @PutMapping("/profile")
+    public ResponseEntity<UserResponseDto> updateProfileImage(
+        @RequestPart("image") MultipartFile multipartFile,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        UserResponseDto responseDto = userService.updateProfileImage(multipartFile,
+            userDetails.getUser());
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/profile")
+    public ResponseEntity<UserResponseDto> deleteProfileImage(
+        @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        userService.deleteProfileImage(userDetails.getUser());
+        return ResponseEntity.noContent().build();
     }
 }
 
--- src/main/java/com/example/dp/domain/user/service/UserService.java
+++ src/main/java/com/example/dp/domain/user/service/UserService.java
@@ -3,10 +3,20 @@ package com.example.dp.domain.user.service;
 import com.example.dp.domain.user.dto.UserCheckCodeRequestDto;
 import com.example.dp.domain.user.dto.UserSendMailRequestDto;
 import com.example.dp.domain.user.dto.request.UserSignupRequestDto;
+import com.example.dp.domain.user.dto.response.UserResponseDto;
+import com.example.dp.domain.user.entity.User;
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
     void signup(UserSignupRequestDto requestDto);
+
     void sendMail(UserSendMailRequestDto requestDto);
+
     Boolean checkCode(UserCheckCodeRequestDto requestDto);
+
+    UserResponseDto updateProfileImage(MultipartFile multipartFile, User user) throws IOException;
+
+    void deleteProfileImage(User user);
 }
