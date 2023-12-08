237532d5113d8ecd293811e75f338db0b7121199
seastamp <seastamp10@gmail.com>
2023-12-09 03:11:29 +0900

    [#86] Feat : MenuSimpleResponseDto 이미지 추가

--- src/main/java/com/example/dp/domain/menu/dto/response/MenuSimpleResponseDto.java
+++ src/main/java/com/example/dp/domain/menu/dto/response/MenuSimpleResponseDto.java
@@ -10,6 +10,8 @@ public class MenuSimpleResponseDto {
     private final String name;
     private final String description;
     private final Integer price;
+    private final String imageName;
+    private final String imagePath;
     private final Integer likeCounts;
 
     public MenuSimpleResponseDto(Menu menu) {
@@ -17,6 +19,8 @@ public class MenuSimpleResponseDto {
         this.name = menu.getName();
         this.description = menu.getDescription();
         this.price = menu.getPrice();
+        this.imageName = menu.getImageName();
+        this.imagePath = menu.getImagePath();
         this.likeCounts = menu.getLikeCounts();
     }
 }
