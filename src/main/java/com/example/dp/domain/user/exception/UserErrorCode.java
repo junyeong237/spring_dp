7e04d92fc5cf6d2766fb4a505eadf4fc80706747
junxtar <aa1870088@naver.com>
2023-12-10 18:30:39 +0900

    [#98] feat: 회원가입, 비밀번호 변경시 비밀번호 기록 로직 적용

--- src/main/java/com/example/dp/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/dp/domain/user/exception/UserErrorCode.java
@@ -14,7 +14,9 @@ public enum UserErrorCode implements ErrorCode {
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
     BLOCK_USER(HttpStatus.UNAUTHORIZED, "차단된 사용자입니다."),
     UNAUTHORIZED_EMAIL(HttpStatus.UNAUTHORIZED, "인증 되지 않은 이메일입니다."),
-    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다.");
+    PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다."),
+    PASSWORD_RESTRICTION(HttpStatus.BAD_REQUEST, "최근 3번안에 사용한 패스워드 입니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
